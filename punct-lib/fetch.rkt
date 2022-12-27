44286d7db5658f6b8abf4917344c6e31e041eacd
Joel Dueck <joel@jdueck.net>
2022-12-27 08:55:37 -0600

    Add get-doc-ref to fetch

--- punct-lib/fetch.rkt
+++ punct-lib/fetch.rkt
@@ -1,8 +1,14 @@
 #lang racket/base
 
-(provide get-doc)
+(require "doc.rkt")
 
-(define (get-doc src)
+(provide get-doc
+         get-doc-ref)
+
+(define (get-doc src [caller 'get-doc])
   (unless (file-exists? src)
     (raise-argument-error 'get-doc "path to existing file" src))
   (dynamic-require src 'doc))
+
+(define (get-doc-ref src key)
+  (hash-ref (document-metas (get-doc src 'get-doc-ref)) key #f))
