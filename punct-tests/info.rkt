68921d0bf9d90ac3437826ea38f3c461b6b84de0
Joel Dueck <joel@jdueck.net>
2025-06-30 09:37:08 -0500

    Fix CI and tests package

--- punct-tests/info.rkt
+++ punct-tests/info.rkt
@@ -2,8 +2,11 @@
 
 (define collection "punct")
 
-(define build-deps '("punct-lib"))
-(define deps '("base"))
+(define build-deps '("scribble-lib"
+                     "punct-lib"))
+(define deps '("punct-lib"
+               "rackunit-lib"
+               "base"))
 (define update-implies '("punct-lib"))
 
 (define pkg-desc "tests part of \"punct\"")
